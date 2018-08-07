18dec3d357dcb429f733d8711dd6d328af1d0fe0
dexfire <275176629@qq.com>
2018-08-07 22:05:11 +0800

    small changes

--- app/src/main/java/work/mathwiki/MainActivity.java
+++ app/src/main/java/work/mathwiki/MainActivity.java
@@ -1,5 +1,6 @@
 package work.mathwiki;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -7,7 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -18,7 +18,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import work.mathwiki.activities.SettingsActivity;
@@ -28,7 +28,8 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private TextView mTextMessage;
     private WebView mWebView;
-    private WikiWebViewClient mWebClient;
+    DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,33 +39,30 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab =  findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+                this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.bottom_navigation);
         navigationView.setNavigationItemSelectedListener(this);
         mTextMessage = findViewById(R.id.message);
         BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setAnimation(null);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         mWebView = findViewById(R.id.main_webview);
         initWebView();
-        mWebView.loadUrl("");
+        mWebView.loadUrl("file://+" + Environment.getExternalStorageDirectory()+"/MathWiki/index.html");
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     private void initWebView() {
-        mWebClient = new WikiWebViewClient();
+        WikiWebViewClient mWebClient = new WikiWebViewClient();
         mWebView.setWebViewClient(mWebClient);
         WebSettings  webSettings = mWebView.getSettings();
         webSettings.setAllowFileAccess(true);
@@ -74,7 +72,6 @@ public class MainActivity extends AppCompatActivity
         // 可能引发跨站脚本攻击(XSS)
         webSettings.setJavaScriptEnabled(true);
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        mWebView.loadUrl("file://"+ Environment.getExternalStorageDirectory()+"/MathWiki/index.html");
     }
 
     @Override
@@ -114,21 +111,26 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        switch(id){
+            case R.id.nav_home:
+                break;
+            case R.id.nav_context:
+                break;
+            case R.id.nav_toybox:
+                break;
+            case R.id.nav_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+            case R.id.nav_goto:
 
-        if (id == R.id.nav_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-        } else if (id == R.id.nav_context) {
-            // TODO: swift to context
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+                break;
+            case R.id.nav_share:
+                break;
+            case R.id.nav_exit:
+                break;
         }
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
