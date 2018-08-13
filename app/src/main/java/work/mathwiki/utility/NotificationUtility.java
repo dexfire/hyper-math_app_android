b426ee91d1a8c241cbd96992fcdf363957a017d1
Dexfire <275176629@qq.com>
2018-08-13 10:57:49 +0800

    finished dialog notification( 虽然效果爆炸~ )

--- app/src/main/java/work/mathwiki/utility/NotificationUtility.java
+++ app/src/main/java/work/mathwiki/utility/NotificationUtility.java
@@ -9,17 +9,21 @@ import android.graphics.BitmapFactory;
 import android.support.annotation.DrawableRes;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatDialog;
+import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import work.mathwiki.MainActivity;
 import work.mathwiki.R;
 
 
 public class NotificationUtility {
+
     private static final String NOTIFICATION_CHANNEL_ID = "mathwiki.work";
     private static final int NOTIFICATION_ID = 2018;
-
+    public static final int DIALOG_NOTIFICATION_PERMISSION_GRANT = 1;
     private PendingIntent getPendingIntent(Context context){
         return PendingIntent.getActivity(context,0,new Intent(context, MainActivity.class),PendingIntent.FLAG_CANCEL_CURRENT);
     }
@@ -40,16 +44,31 @@ public class NotificationUtility {
         notificationManager.notify(2018,notification);
     }
 
-    public void makeDialogNotification(Context context,String title, String subtitle, int type){
+    public static void makeDialogNotification(Context context,String title, String subtitle, int type){
         AppCompatDialog dialog = new AppCompatDialog(context);
-        dialog.setTitle(title);
-        dialog.setContentView(android.R.layout.select_dialog_item);
-        TextView tv = dialog.findViewById(android.R.id.text1);
-        if(tv!=null)
-        tv.setText(R.string.notification_test);
-        dialog.setCancelable(true);
-        dialog.setFeatureDrawable(AppCompatDialog.BUTTON_POSITIVE,context.getDrawable(R.drawable.ic_circle));
-        dialog.setFeatureDrawable(AppCompatDialog.BUTTON_NEGATIVE,context.getDrawable(R.drawable.ic_clear));
+        TextView textView;
+        switch (type){
+            case DIALOG_NOTIFICATION_PERMISSION_GRANT:
+                dialog.supportRequestWindowFeature(Window.FEATURE_LEFT_ICON);
+                dialog.setFeatureDrawable(Window.FEATURE_LEFT_ICON,context.getDrawable(R.drawable.ic_circle));
+                dialog.setTitle(R.string.permission_apply);
+                textView = new TextView(context);
+                textView.setText(subtitle);
+                dialog.setContentView(textView);
+                dialog.setCancelable(true);
+                break;
+            default:
+//                dialog.supportRequestWindowFeature(AppCompatDialog.BUTTON_POSITIVE);
+//                dialog.supportRequestWindowFeature(AppCompatDialog.BUTTON_NEGATIVE);
+//                dialog.setFeatureDrawable(AppCompatDialog.BUTTON_POSITIVE,context.getDrawable(R.drawable.ic_circle));
+//                dialog.setFeatureDrawable(AppCompatDialog.BUTTON_NEGATIVE,context.getDrawable(R.drawable.ic_clear));
+                dialog.setTitle(title);
+                dialog.setContentView(android.R.layout.select_dialog_item);
+                textView = dialog.findViewById(android.R.id.text1);
+                if(textView!=null)
+                    textView.setText(R.string.notification_test);
+                dialog.setCancelable(true);
+        }
         dialog.show();
     }
 
