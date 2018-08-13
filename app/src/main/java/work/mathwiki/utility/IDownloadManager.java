4f8e4d1a9019c163de0dd5a8b585fe0454175fce
Dexfire <275176629@qq.com>
2018-08-13 10:52:40 +0800

    implement some method..........

--- app/src/main/java/work/mathwiki/utility/IDownloadManager.java
+++ app/src/main/java/work/mathwiki/utility/IDownloadManager.java
@@ -4,14 +4,21 @@ import android.app.DownloadManager;
 import android.content.Context;
 import android.net.Uri;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.cert.Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLPeerUnverifiedException;
+
 import retrofit2.Retrofit;
 
-/**
- * Created by Dexfire on 2018/8/5 0005.
- */
 
 public class IDownloadManager {
-    private static final String baseUrl = "http://";
+    private static final String baseUrl = "http://api.github.com/repos/dexfire/mathwiki/releases/latest";
     private static IDownloadManager singleInstance = null;
     private static Context context;
     public static IDownloadManager getInstance(Context context) {
@@ -25,11 +32,61 @@ public class IDownloadManager {
     private IDownloadManager(Context context) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
-
                 .build();
     }
 
+    private static void downloadOverHttps(){
+        // on okhttp: create a named Runnable
+        new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("https","api.github.com","/repos/dexfire/mathwiki/releases/latest");
+                    HttpsURLConnection connection = new HttpsURLConnection(url) {
+                        @Override
+                        public String getCipherSuite() {
+                            return null;
+                        }
+
+                        @Override
+                        public Certificate[] getLocalCertificates() {
+                            return new Certificate[0];
+                        }
+
+                        @Override
+                        public Certificate[] getServerCertificates() throws SSLPeerUnverifiedException {
+                            return new Certificate[0];
+                        }
+
+                        @Override
+                        public void disconnect() {
+
+                        }
+
+                        @Override
+                        public boolean usingProxy() {
+                            return false;
+                        }
+
+                        @Override
+                        public void connect() throws IOException {
+
+                        }
+                    };
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+
+
+
+    }
+
     public static void downloadWithSystemDM(String link){
+
         Uri uri =  Uri.parse(link);
 //        if(uri.isHierarchical())
 //        DownloadManager dm = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
