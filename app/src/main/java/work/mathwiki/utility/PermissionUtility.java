6dcf0d7b5881811e1b232d4e94ac94d8ff46a8e9
Dexfire <275176629@qq.com>
2018-08-13 10:55:12 +0800

    add permission check when main Activity starts

--- app/src/main/java/work/mathwiki/utility/PermissionUtility.java
+++ app/src/main/java/work/mathwiki/utility/PermissionUtility.java
@@ -1,19 +1,40 @@
 package work.mathwiki.utility;
 
-import android.content.Context;
+import android.Manifest;
+import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.PermissionChecker;
+
+import work.mathwiki.R;
 
 public class PermissionUtility {
 
-    public boolean dialogPermission(Context context,String permission){
-        //TODO: not implemented func
+    private static final String[] permissions = {
+            //Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    };
+
+    public static void checkAllNeededPermissions(Activity activity){
+        String[] notes = activity.getResources().getStringArray(R.array.permissions_notation);
+        for(int i=0;i<permissions.length;i++){
+//            if(i==0)//checkPermission(activity,permissions[i],notes[i]);
+//                else
+            checkPermission(activity,permissions[i],notes[i]);
+        }
+
+    }
+
+    private static boolean dialogPermission(Activity activity, String permission){
+        ActivityCompat.requestPermissions(activity,permissions, PermissionChecker.PERMISSION_GRANTED);
         return false;
     }
 
-    public boolean checkPermission(Context context,String permission){
-        int result = PermissionChecker.checkSelfPermission(context,permission);
+    public static boolean checkPermission(Activity activity,String permission,String note){
+        int result = PermissionChecker.checkSelfPermission(activity,permission);
         if(result!=PermissionChecker.PERMISSION_GRANTED){
-            return dialogPermission(context,permission);
+            NotificationUtility.makeDialogNotification(activity,"",note,NotificationUtility.DIALOG_NOTIFICATION_PERMISSION_GRANT);
+            return dialogPermission(activity,permission);
         }
         return true;
     }
