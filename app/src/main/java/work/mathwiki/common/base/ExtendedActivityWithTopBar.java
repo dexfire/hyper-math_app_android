35f865446fc11679f580edb5e17599955b5915cd
Dexfire <275176629@qq.com>
2018-09-02 20:33:07 +0800

    extend the base Activity functions

---
+++ app/src/main/java/work/mathwiki/common/base/ExtendedActivityWithTopBar.java
@@ -0,0 +1,45 @@
+package work.mathwiki.common.base;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import work.mathwiki.R;
+import work.mathwiki.core.logger.Logger;
+import work.mathwiki.views.TopBarView;
+
+/**
+ * Created by Dexfire on 2018/8/29 0029.
+ */
+
+public class ExtendedActivityWithTopBar extends ExtendedActivity {
+    private TopBarView mTopBarView;
+
+
+    @Override
+    public void setContentView(int layoutResID) {
+        RelativeLayout root = (RelativeLayout) getLayoutInflater().inflate(R.layout.activity_root_layout,null);
+        mTopBarView = root.findViewById(R.id.topbar);
+        initTopBarView();
+        getLayoutInflater().inflate(layoutResID,root.findViewById(R.id.base_root_container));
+        super.setContentView(root);
+    }
+
+    @Override
+    public void setContentView(View view) {
+        RelativeLayout root = (RelativeLayout) getLayoutInflater().inflate(R.layout.activity_root_layout,null);
+        mTopBarView = root.findViewById(R.id.topbar);
+        initTopBarView();
+        ((ViewGroup)(root.findViewById(R.id.base_root_container))).addView(view);
+        super.setContentView(root);
+    }
+
+    private void initTopBarView() {
+        Logger.sd("Init TopBarView Internal");
+
+    }
+}
