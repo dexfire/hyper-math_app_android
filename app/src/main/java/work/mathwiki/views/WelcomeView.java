8667e258424f5e9130542d08e6a6df48316d4f11
Dexfire <275176629@qq.com>
2018-09-02 20:26:59 +0800

    add a dummy Welcome View

---
+++ app/src/main/java/work/mathwiki/views/WelcomeView.java
@@ -0,0 +1,16 @@
+package work.mathwiki.views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class WelcomeView extends LinearLayout {
+    public WelcomeView(Context context) {
+        super(context);
+    }
+
+    public WelcomeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
