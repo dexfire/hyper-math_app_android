894742bde2a3f01d4e1e8f385b8cb77444c5b536
Dexfire <275176629@qq.com>
2018-09-30 23:48:49 +0800

    it will be removed, already have a seprate library.

--- app/src/main/java/work/mathwiki/views/MarkdownEditor.java
+++ app/src/main/java/work/mathwiki/views/MarkdownEditor.java
@@ -2,10 +2,76 @@ package work.mathwiki.views;
 
 import android.content.Context;
 import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatTextView;
 import android.text.Editable;
+import android.text.Selection;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.text.method.ArrowKeyMovementMethod;
+import android.text.method.MovementMethod;
 import android.util.AttributeSet;
 
 public class MarkdownEditor extends AppCompatEditText {
+
+    private static final String TAG = "MarkdownEditorView";
+
+    private TextWatcher mMDFormatter = new TextWatcher(){
+        /**
+         * This method is called to notify you that, within <code>s</code>,
+         * the <code>count</code> characters beginning at <code>start</code>
+         * are about to be replaced by new text with length <code>after</code>.
+         * It is an error to attempt to make changes to <code>s</code> from
+         * this callback.
+         *
+         * @param s
+         * @param start
+         * @param count
+         * @param after
+         */
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        /**
+         * This method is called to notify you that, within <code>s</code>,
+         * the <code>count</code> characters beginning at <code>start</code>
+         * have just replaced old text that had length <code>before</code>.
+         * It is an error to attempt to make changes to <code>s</code> from
+         * this callback.
+         *
+         * @param s
+         * @param start
+         * @param before
+         * @param count
+         */
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        /**
+         * This method is called to notify you that, somewhere within
+         * <code>s</code>, the text has been changed.
+         * It is legitimate to make further changes to <code>s</code> from
+         * this callback, but be careful not to get yourself into an infinite
+         * loop, because any changes you make will cause this method to be
+         * called again recursively.
+         * (You are not told where the change took place because other
+         * afterTextChanged() methods may already have made other changes
+         * and invalidated the offsets.  But if you need to know here,
+         * you can use {@link android.text.Spannable#setSpan} in {@link #onTextChanged}
+         * to mark your place and then look up from here where the span
+         * ended up.
+         *
+         * @param s
+         */
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
     public MarkdownEditor(Context context) {
         super(context,null);
     }
@@ -16,9 +82,73 @@ public class MarkdownEditor extends AppCompatEditText {
 
     public MarkdownEditor(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        addTextChangedListener(mMDFormatter);
     }
 
-    public int getCursorPosition(){
-        return 0;
+    @Override
+    public boolean getFreezesText() {
+        return true;
     }
+
+    @Override
+    protected boolean getDefaultEditable() {
+        return true;
+    }
+
+    @Override
+    protected MovementMethod getDefaultMovementMethod() {
+        return ArrowKeyMovementMethod.getInstance();
+    }
+
+    @Override
+    public Editable getText() {
+        return (Editable) super.getText();
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        super.setText(text, BufferType.EDITABLE);
+    }
+
+    @Override
+    public void setSelection(int start, int stop) {
+        Selection.setSelection(getText(), start, stop);
+    }
+
+    public void setSelection(int index) {
+        Selection.setSelection(getText(), index);
+    }
+
+    public void selectAll() {
+        Selection.selectAll(getText());
+    }
+
+    public void extendSelection(int index) {
+        Selection.extendSelection(getText(), index);
+    }
+
+    /**
+     * Causes words in the text that are longer than the view's width to be ellipsized instead of
+     * broken in the middle. {@link TextUtils.TruncateAt#MARQUEE
+     * TextUtils.TruncateAt#MARQUEE} is not supported.
+     *
+     * @param ellipsis Type of ellipsis to be applied.
+     * @throws IllegalArgumentException When the value of <code>ellipsis</code> parameter is
+     *      {@link TextUtils.TruncateAt#MARQUEE}.
+     * @see android.widget.TextView#setEllipsize(TextUtils.TruncateAt)
+     */
+    @Override
+    public void setEllipsize(TextUtils.TruncateAt ellipsis) {
+        if (ellipsis == TextUtils.TruncateAt.MARQUEE) {
+            throw new IllegalArgumentException("EditText cannot use the ellipsize mode "
+                    + "TextUtils.TruncateAt.MARQUEE");
+        }
+        super.setEllipsize(ellipsis);
+    }
+
+    @Override
+    public CharSequence getAccessibilityClassName() {
+        return MarkdownEditor.class.getName();
+    }
+
 }
