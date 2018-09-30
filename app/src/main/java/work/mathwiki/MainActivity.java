ecca68fa05c390ece7e5a31910d00478d82c8c74
Dexfire <275176629@qq.com>
2018-09-30 23:51:11 +0800

    simplified onCreate() struct

--- app/src/main/java/work/mathwiki/MainActivity.java
+++ app/src/main/java/work/mathwiki/MainActivity.java
@@ -1,24 +1,19 @@
 package work.mathwiki;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,24 +23,27 @@ import android.webkit.WebView;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 
+import com.exyui.android.debugbottle.components.DTInstaller;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
 import java.io.File;
 
 import work.mathwiki.activities.SettingsActivity;
 import work.mathwiki.activities.GuideSplashActivity;
 import work.mathwiki.activities.ShareAppActivity;
-import work.mathwiki.core.content.ContentManager;
-import work.mathwiki.core.content.ContentViewsEnum;
-import work.mathwiki.core.content.LocalFileContentProvider;
+import work.mathwiki.common.base.ExtendedActivity;
+import work.mathwiki.core.settings.SettingsManager;
+import work.mathwiki.core.viewmodel.ContentManager;
+import work.mathwiki.core.viewmodel.ContentViewsEnum;
+import work.mathwiki.core.webview.LocalFileContentProvider;
 import work.mathwiki.core.data.DataManager;
 import work.mathwiki.core.logger.Logger;
-import work.mathwiki.core.network.AppUpdateInfo;
-import work.mathwiki.core.network.IDownloadManager;
 import work.mathwiki.core.webview.LocalWebViewClient;
 import work.mathwiki.updater.AppUpdateManager;
 import work.mathwiki.utility.ConstFieleds;
 import work.mathwiki.utility.NotificationUtility;
 import work.mathwiki.utility.PermissionUtility;
-import work.mathwiki.utility.WebViewSetup;
+import work.mathwiki.utility.WebViewUtil;
 
 /***
  *  Hyper-Math
@@ -63,7 +61,7 @@ import work.mathwiki.utility.WebViewSetup;
  */
 
 @SuppressLint("SetTextI18n")
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends ExtendedActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     public static final String Package_Name = "work.mathwiki";
     public static final String PRODUCT_NAME = "Hyper-Math";
@@ -85,21 +83,41 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Logger.setDebug(true);
         log = Logger.build("MainActivity");
         handler = new Handler();
-        // TODO : 启动浮窗、非全屏、意蕴&装逼向
 
-        // 判断、启动介绍页
-        SharedPreferences preferences = getSharedPreferences(getPackageName(), MODE_PRIVATE);
-        boolean firstStarted =  preferences.getBoolean(ConstFieleds.Preference_Showed_Welcome_Page,false);
-        if(!firstStarted) {
-            log.ii("Starting UserGuideSplashActivity");
-            startActivity(new Intent(this, GuideSplashActivity.class));
+        if(Logger.DEBUG){
+            DTInstaller.install(getApplication())
+                    //.setOkHttpClient(httpClient)
+                    //.setPackageName("your.package")
+                    .enable()
+                    .run();
         }
 
-        // 布局 Views
+        SettingsManager.init(this);
+
+        // TODO : 启动浮窗、非全屏、意蕴&装逼向
+
+        checkStartWelcomeSplash();        // 判断、启动介绍页
+        initConstsField();        // 获取一些常用的全局变量
+        initMainLayout();        // 布局 Views
+        PermissionUtility.checkAllNeededPermissions(this);        // check permission of storage usage
+        AppUpdateManager.getInstance().autoCheckUpdates(this);        // check updates
+
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//            transaction.add(R.id.main_container,new BrowserFragment(),BrowserFragment.TAG)
+//                //.addToBackStack(null)
+//                .commitNowAllowingStateLoss();
+//        getSupportFragmentManager().executePendingTransactions();
+//        showFragment(BrowserFragment.TAG);
+
+//        View statusBar = findViewById(android.R.id.statusBarBackground);
+//        statusBar.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+    }
+
+    private void initMainLayout() {
         // 默认显示主页
         log.dd("Main Activity starting");
         ContentManager.addView(ContentViewsEnum.home,(ViewGroup) getLayoutInflater().inflate(R.layout.layout_home,null),mIndexCallBacks);
-        ContentManager.addView(ContentViewsEnum.context,(ViewGroup) getLayoutInflater().inflate(R.layout.layout_context,null),mContextCallBacks);
+        //ContentManager.addView(ContentViewsEnum.context,(ViewGroup) getLayoutInflater().inflate(R.layout.layout_context,null),mContextCallBacks);
         ContentManager.addView(ContentViewsEnum.toys,(ViewGroup) getLayoutInflater().inflate(R.layout.layout_toysbox,null),mToyBoxCallBacks);
         setContentView(R.layout.activity_main);
         mContainer = findViewById(R.id.activity_main_container);
@@ -113,43 +131,46 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //toolbar.setLogo(R.drawable.ic_arrow_back);
         setSupportActionBar(toolbar);
 
-        // 浮动按钮初始化
-        FloatingActionButton fab =  findViewById(R.id.fab);
-            fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
+//        // 浮动按钮初始化
+//        FloatingActionButton fab =  findViewById(R.id.fab);
+//            fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show());
 
         // 侧拉菜单初始化
         mDrawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-            mDrawerLayout.addDrawerListener(toggle);
-            toggle.syncState();
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
 
         // 底部导航三键
 
         NavigationView navigationView = findViewById(R.id.bottom_navigation);
-            navigationView.setNavigationItemSelectedListener(this);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-            navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-            navigation.setStateListAnimator(null);
+        navigationView.setNavigationItemSelectedListener(this);
+        BottomNavigationViewEx bnv = findViewById(R.id.navigation);
+        bnv.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bnv.setStateListAnimator(null);
+    }
 
+    private void checkStartWelcomeSplash() {
+        if(! SettingsManager.getInstance().isGeneral_First_Welcome_Splash_Showed()){
+            log.ii("Starting UserGuideSplashActivity");
+            startActivity(new Intent(this, GuideSplashActivity.class));
+        }
+        SharedPreferences preferences = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+        boolean firstStarted =  preferences.getBoolean(ConstFieleds.Preference_Showed_Welcome_Page,false);
+        if(!firstStarted) {
 
-         // 权限检查
-        //TODO : check permission of storage usage
-        PermissionUtility.checkAllNeededPermissions(this);
-        //TODO : check update
-        AppUpdateManager.getInstance().checkUpdates(this);
+        }
+    }
 
-//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//            transaction.add(R.id.main_container,new BrowserFragment(),BrowserFragment.TAG)
-//                //.addToBackStack(null)
-//                .commitNowAllowingStateLoss();
-//        getSupportFragmentManager().executePendingTransactions();
-//        showFragment(BrowserFragment.TAG);
+    private void initConstsField() {
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+        ConstFieleds.Screen_Width = point.x;
+        ConstFieleds.Screen_Height = point.y;
 
-//        View statusBar = findViewById(android.R.id.statusBarBackground);
-//        statusBar.setBackgroundColor(getResources().getColor(R.color.colorAccent));
     }
 
     @Override
@@ -169,10 +190,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 NotificationUtility.makeShortToast(this,getResources().getString(R.string.press_again_to_exit));
             }
         }
-    }
-
-    public static Handler getHandler(){
-        return handler;
     }
 
     @Override
@@ -204,8 +221,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_home:
                 ContentManager.showContent(ContentViewsEnum.home,mContainer);
                 break;
-            case R.id.nav_context:
-                ContentManager.showContent(ContentViewsEnum.context,mContainer);
+            case R.id.nav_note:
+                //ContentManager.showContent(ContentViewsEnum.context,mContainer);
                 break;
             case R.id.nav_toybox:
                 ContentManager.showContent(ContentViewsEnum.toys,mContainer);
@@ -255,15 +272,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     // 底部导航键 选择事件
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    private BottomNavigationViewEx.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = item -> {
                 switch (item.getItemId()) {
                     case R.id.navigation_home:
                         //TODO : 跳转逻辑
                         ContentManager.showContent(ContentViewsEnum.home,mContainer);
                         return true;
-                    case R.id.navigation_context:
-                        ContentManager.showContent(ContentViewsEnum.context,mContainer);
+                    case R.id.navigation_note:
+                        //ContentManager.showContent(ContentViewsEnum.context,mContainer);
                         return true;
                     case R.id.navigation_toybox:
                         ContentManager.showContent(ContentViewsEnum.toys,mContainer);
@@ -287,7 +304,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         @Override
         public void onInit(ContentViewsEnum key, ViewGroup view) {
             webView = view.findViewById(R.id.layout_index_webview);
-            WebViewSetup.initializeWebView(webView);
+            WebViewUtil.initializeWebView(webView);
             webView.setWebViewClient(new LocalWebViewClient());
             addr_text = view.findViewById(R.id.home_addr);
             addr_text.setText(LocalFileContentProvider.URI_PREFIX + File.separator + "index.html");
@@ -340,7 +357,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         public void onInit(ContentViewsEnum key, ViewGroup view) {
             webView = view.findViewById(R.id.layout_context_webview);
 
-            WebViewSetup.initializeWebView(webView);
+            WebViewUtil.initializeWebView(webView);
             client = new LocalWebViewClient();
             webView.setWebViewClient(client);
         }
@@ -390,8 +407,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         @Override
         public void onShow(ContentViewsEnum key, ViewGroup view) {
-            //IDownloadManager.downloadOverHttps(MainActivity.this);
-            AppUpdateManager.getInstance().checkUpdates(MainActivity.this);
+            //IDownloadManager.downloadOverHttp(MainActivity.this);
+            AppUpdateManager.getInstance().autoCheckUpdates(MainActivity.this);
         }
 
         @Override
