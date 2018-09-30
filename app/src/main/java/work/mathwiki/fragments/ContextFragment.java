0b1b18261364f0457431f74bfee6dc0068e12c8a
Dexfire <275176629@qq.com>
2018-09-30 23:51:52 +0800

    nothing

--- app/src/main/java/work/mathwiki/fragments/ContextFragment.java
+++ app/src/main/java/work/mathwiki/fragments/ContextFragment.java
@@ -28,7 +28,7 @@ public class ContextFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the toast_info for this fragment
         return inflater.inflate(R.layout.layout_context, container, false);
     }
 
