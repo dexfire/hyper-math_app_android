1d55458ad636c0f73b7853f2314e536348d260b8
Dexfire <275176629@qq.com>
2018-09-30 23:53:10 +0800

    nothing, may be removed

--- app/src/main/java/work/mathwiki/activities/CollectionsActivity.java
+++ app/src/main/java/work/mathwiki/activities/CollectionsActivity.java
@@ -14,6 +14,6 @@ public class CollectionsActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout);
+//        setContentView(R.toast_info);
     }
 }
