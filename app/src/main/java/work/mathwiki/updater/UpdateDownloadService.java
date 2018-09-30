6c4c83008f35df4148cdcc4389726bba0588390d
Dexfire <275176629@qq.com>
2018-09-30 23:50:39 +0800

    remove javadocs

--- app/src/main/java/work/mathwiki/updater/UpdateDownloadService.java
+++ app/src/main/java/work/mathwiki/updater/UpdateDownloadService.java
@@ -20,20 +20,11 @@ public class UpdateDownloadService extends Service {
         super();
     }
 
-    /**
-     * Called by the system when the service is first created.  Do not call this method directly.
-     */
     @Override
     public void onCreate() {
         super.onCreate();
     }
 
-    /**
-     * Called by the system to notify a Service that it is no longer used and is being removed.  The
-     * service should clean up any resources it holds (threads, registered
-     * receivers, etc) at this point.  Upon return, there will be no more calls
-     * in to this Service object and it is effectively dead.  Do not call this method directly.
-     */
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -54,74 +45,23 @@ public class UpdateDownloadService extends Service {
         super.onTrimMemory(level);
     }
 
-    /**
-     * Called when all clients have disconnected from a particular interface
-     * published by the service.  The default implementation does nothing and
-     * returns false.
-     *
-     * @param intent The Intent that was used to bind to this service,
-     *               as given to {@link Context#bindService
-     *               Context.bindService}.  Note that any extras that were included with
-     *               the Intent at that point will <em>not</em> be seen here.
-     * @return Return true if you would like to have the service's
-     * {@link #onRebind} method later called when new clients bind to it.
-     */
+
     @Override
     public boolean onUnbind(Intent intent) {
         return super.onUnbind(intent);
     }
 
-    /**
-     * Called when new clients have connected to the service, after it had
-     * previously been notified that all had disconnected in its
-     * {@link #onUnbind}.  This will only be called if the implementation
-     * of {@link #onUnbind} was overridden to return true.
-     *
-     * @param intent The Intent that was used to bind to this service,
-     *               as given to {@link Context#bindService
-     *               Context.bindService}.  Note that any extras that were included with
-     *               the Intent at that point will <em>not</em> be seen here.
-     */
     @Override
     public void onRebind(Intent intent) {
         super.onRebind(intent);
     }
 
-    /**
-     * This is called if the service is currently running and the user has
-     * removed a task that comes from the service's application.  If you have
-     * set {@link ServiceInfo#FLAG_STOP_WITH_TASK ServiceInfo.FLAG_STOP_WITH_TASK}
-     * then you will not receive this callback; instead, the service will simply
-     * be stopped.
-     *
-     * @param rootIntent The original root Intent that was used to launch
-     *                   the task that is being removed.
-     */
     @Override
     public void onTaskRemoved(Intent rootIntent) {
         super.onTaskRemoved(rootIntent);
     }
 
-    /**
-     * Return the communication channel to the service.  May return null if
-     * clients can not bind to the service.  The returned
-     * {@link IBinder} is usually for a complex interface
-     * that has been <a href="{@docRoot}guide/components/aidl.html">described using
-     * aidl</a>.
-     * <p>
-     * <p><em>Note that unlike other application components, calls on to the
-     * IBinder interface returned here may not happen on the main thread
-     * of the process</em>.  More information about the main thread can be found in
-     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html">Processes and
-     * Threads</a>.</p>
-     *
-     * @param intent The Intent that was used to bind to this service,
-     *               as given to {@link Context#bindService
-     *               Context.bindService}.  Note that any extras that were included with
-     *               the Intent at that point will <em>not</em> be seen here.
-     * @return Return an IBinder through which clients can call on to the
-     * service.
-     */
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
